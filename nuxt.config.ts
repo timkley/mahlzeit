2dfe1af16230425fea94adb210ca1b0bf601017f
Tim Kleyersburg <tk@wacg.de>
2026-03-26 20:14:13 +0100

    Disable iOS input zoom by restricting viewport scaling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,6 +38,12 @@ export default defineNuxtConfig({
     viewTransition: true,
   },
 
+  app: {
+    head: {
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
+    },
+  },
+
   future: {
     compatibilityVersion: 4,
   },
